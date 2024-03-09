e3b4135b2e014f102e679feef6ef216346dc589e
Gouranshvaishnavji <gouransh.v@kalvium.community>
2024-03-09 11:49:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get(
+          'https://reactnd-books-api.udacity.com/books',
+          {
+            headers: {
+              Authorization: 'whatever-you-want',
+            },
+          }
+        );
+        console.log(response);
+        const fetchedBooks = response.data.books;
+        setBooks(fetchedBooks);
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+return (
+  <>
+      {books.map((book) => (
+        <div className="book-card" key={book.id}>
+          <div className="image-container">
+            <img src={book.imageLinks.thumbnail} alt={book.title} className="book-image" />
+          </div>
+          <div className="text-container">
+            <h1 className="book-title">{book.title}</h1>
+            <h3 className="book-authors">By {book.authors}</h3>
+            <article className="book-description">{book.description}</article>
+          </div>
+        </div>
+      ))}
+      
+  </>
+);
+}
+export default App
